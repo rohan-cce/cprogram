22cb7a8e4d2b8e96fc7479ed8cc4b0f77c07764a
Rohan j <59871907+rohan-cce@users.noreply.github.com>
2020-10-18 12:53:06 +0530

    Create floyd_triangle.c

---
+++ floyd_triangle.c
@@ -0,0 +1,18 @@
+// Without using a temporary variable and with only one loop 
+#include<stdio.h> 
+void floyd(n){ 
+	int i,j=1; 
+	for (i=1;i<=(n*(n+1))/2;i++){ 
+		printf("%d ",i); 
+		if(i==(j*(j+1))/2){ 
+			printf("\n"); 
+			j++; 
+		} 
+	} 
+} 
+
+int main(){ 
+	floyd(6); 
+} 
+
+//This code is contributed by Vishal B 
